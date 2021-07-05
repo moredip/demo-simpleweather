7a1ee7dbfaea2757655260d38e9b3fc7c7808c62
Pete Hodgson <git@thepete.net>
2021-07-04 17:26:53 -0700

    disable DEBUG unless turned on via env var

--- simpleweather/settings.py
+++ simpleweather/settings.py
@@ -10,7 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
-from os import environ
+from os import getenv
 from pathlib import Path
 
 import dj_database_url
@@ -22,14 +22,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-SECRET_KEY = environ.get('SECRET_KEY', 'django-insecure')
+SECRET_KEY = getenv('SECRET_KEY', 'django-insecure')
 
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = getenv("DEBUG", 'false').lower() in ('true', '1', 't')
 
-ALLOWED_HOSTS = []
 
+ALLOWED_HOSTS = ['example-simpleweather.herokuapp.com','localhost','127.0.0.1']
 
 # Application definition
 
@@ -91,5 +90,4 @@ STATIC_URL = '/static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-ALLOWED_HOSTS = ['example-simpleweather.herokuapp.com','localhost','127.0.0.1']
 
